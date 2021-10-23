332bc83ecc19b339ece48f9a1fcc589f94654e31
Tlalvani <trlalvani@gmail.com>
2021-10-23 14:01:38 -0400

    Added patient database

---
+++ pitt_challenge/lib/screens/auth/Auth.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:pitt_challenge/screens/auth/Register.dart';
+import 'package:pitt_challenge/screens/auth/SignIn.dart';
+class Auth extends StatefulWidget {
+  @override
+  _AuthState createState() => _AuthState();
+}
+
+class _AuthState extends State<Auth> {
+  bool showSignIn = true;
+  void toggleView(){
+    //print(showSignIn.toString());
+    setState(() => showSignIn = !showSignIn);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (showSignIn) {
+      return SignIn(toggleView:  toggleView);
+    } else {
+      return Register(toggleView:  toggleView);
+    }
+  }
+}
---
+++ pitt_challenge/lib/services/db.dart
@@ -0,0 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../screens/patientScreens/patient.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:pitt_challenge/services/authFB.dart';
+
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+class DbService{
+
+  final String uid;
+  DbService({this.uid});
+  final CollectionReference doctorData = FirebaseFirestore.instance.collection('Professionals');
+
+  String UID = (_auth.currentUser.uid);
+
+  Stream<QuerySnapshot> getStream() {
+    return doctorData.doc(UID).collection('Patients').snapshots();
+  }
+
+  Future<DocumentReference> addPatient(Patient patient) {
+    return doctorData.doc(UID).collection('Patients').add(patient.toJson());
+  }
+
+  void updatePatient(Patient patient) async {
+    await doctorData.doc(UID).collection('Patients').doc(patient.referenceID).update(patient.toJson());
+  }
+
+  void deletePatient(Patient patient) async {
+    await doctorData.doc(UID).collection('Patients').doc(patient.referenceID).delete();
+  }
+
+}
+
